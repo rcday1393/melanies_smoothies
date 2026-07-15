00713f4d1c7b7813a39ddc355054769fbe93a41e
rcday1393 <riley.day@kwa-analytics.com>
2026-07-15 10:14:10 -0500

    Refactor Snowflake connection and add cryptography import
    Updated Snowflake connection handling and added import for cryptography.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import snowflake.connector
+from cryptography.hazmat.primitives import serialization
+
+
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw: {st.__version__}")
@@ -15,14 +19,7 @@ st.write("The name on your smoothie will be: ", name_on_order)
 
 
 # Get the current credentials
-# cnx = st.connection("snowflake")
-conn = st.connection(
-    "",
-    type="snowflake",
-    account="CYWTBCU-AKB53809",
-    user="rcday",
-    authenticator="externalbrowser",
-)
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
