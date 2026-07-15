ea5acecb147e71a7d09d39b6520d3d1af7481b6a
rcday1393 <riley.day@kwa-analytics.com>
2026-07-15 09:35:57 -0500

    Add Snowflake connection setup in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,13 @@ st.write("The name on your smoothie will be: ", name_on_order)
 
 # Get the current credentials
 cnx = st.connection("snowflake")
+conn = st.connection(
+    "",
+    type="snowflake",
+    account="CYWTBCU-AKB53809",
+    user="rcday",
+    authenticator="externalbrowser",
+)
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
